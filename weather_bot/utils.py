1a0008989e31bb098abdde9af48d9c5ffebcd3c5
0140454 <s1994928@yahoo.com.tw>
2016-12-15 14:14:30 +0800

    Initialize new Django project

---
+++ weather_bot/utils.py
@@ -0,0 +1,8 @@
+import os
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = 'Set the {} environment variable'.format(var_name)
+        raise ImproperlyConfigured(error_msg)
